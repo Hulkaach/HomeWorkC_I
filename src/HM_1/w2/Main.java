5cf4b63fc93881b53914601898258b12aec8b11b
DESKTOP-QQACB3T\Chester <zolinanata2006@mail.ru>
2023-11-27 19:44:49 +0300

    третья попытка

---
+++ src/HM_1/w2/Main.java
@@ -0,0 +1,12 @@
+package HM_1.w2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("**************************");
+        System.out.println("****\t  Визитка \t  ****");
+        System.out.println("**** Name: you name\t  ****");
+        System.out.println("**** Skype: you skype ****");
+        System.out.println("**************************");
+
+    }
+}
